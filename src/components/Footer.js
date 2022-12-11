a79755f2a3b70b23ce9adb67c6da2d8260de8da9
kanisshka <kanisshka12@gmail.com>
2022-12-11 21:55:42 +0530

    h

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import flogo from '../assets/llogo.png'
+
+const Footer = () => {
+    return (
+        <>
+            <section className='footer w-full h-[865px] bg-no-repeat bg-center bg-cover  overflow-hidden'>
+                <div className="container-fluid flex flex-col items-center justify-between">
+                    
+                    
+                        <p className='foot-head'>Make every day happier</p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Footer
